b3ccbe9c427edc9da3c843e4d3c3fc5c8f1ba5df
raseelaldawish <152087958+raseelaldawish@users.noreply.github.com>
2024-10-23 18:08:48 +0300

    Update controlProfileModel.js
    from attribute power:string to isPowerdOn:Boolean

--- database- sprint1-week2/models/controlProfileModel.js
+++ database- sprint1-week2/models/controlProfileModel.js
@@ -1,39 +1,31 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 //collection ControlProfile schema
-const controlProfileSchema = new mongoose.Schema(
-    {
-        controlNodeID: {    //refernces the control node connected to
-            type: mongoose.Schema.ObjectId,
-            ref: 'controlnodes', 
-            default: null
-        },
-        startTime:{
-            type:Date
-        }, 
-        endTime:{
-            type:Date
-        },
-        startDate:{
-            type: Date
-        },
-        endDate:{
-            type:Date
-        },
-        BrightnessLevel:{  //brightness level percentage of the luminaire
-            type: Number,
-            required: [true, 'A street light must have a brightness level.'] 
-        },
-        power:{      //current operational state
-            type: String,
-            enum: {
-              values: ['on', 'off'],
-              message: 'the streetlight is either: on or off'
-            },
-            default: 'off'
-        }
-    }
-);
+const controlProfileSchema = new mongoose.Schema({
+  controlNodeID: {
+    //refernces the control node connected to
+    type: mongoose.Schema.ObjectId,
+    ref: "controlnodes",
+    default: null,
+  },
+  startTimestamp: {
+    type: Date,
+  },
+  endTimestamp: {
+    type: Date,
+  },
+  BrightnessLevel: {
+    //brightness level percentage of the luminaire
+    type: Number,
+    required: [true, "A street light must have a brightness level."],
+  },
+  isPoweredOn: {
+    //current operational state
+    type: Boolean,
+    default: "off",
+  },
+});
+
 //creating a model for ControlProfile collection
-const ControlProfile= mongoose.model('ControlProfile',controlProfileSchema);
+const ControlProfile = mongoose.model("ControlProfile", controlProfileSchema);
 module.exports = ControlProfile;
